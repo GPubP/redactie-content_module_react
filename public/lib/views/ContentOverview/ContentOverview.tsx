2791cf5edb4d0fce31e3699f22b552bf03ed02c2
maximdegeyter <maximdegeyter@outlook.com>
2020-03-31 12:05:13 +0200

    feat: add context header

--- public/lib/views/ContentOverview/ContentOverview.tsx
+++ public/lib/views/ContentOverview/ContentOverview.tsx
@@ -1,10 +1,22 @@
-import { ContextHeader } from '@acpaas-ui/react-editorial-components';
+import { Button } from '@acpaas-ui/react-components';
+import {
+	ContextHeader,
+	ContextHeaderActionsSection,
+	ContextHeaderTopSection,
+} from '@acpaas-ui/react-editorial-components';
 import React, { FC } from 'react';
 
 import { ContentRouteProps } from '../../content.types';
 
 const ContentOverview: FC<ContentRouteProps> = ({ basePath }) => {
-	return <ContextHeader title="Content overzicht" />;
+	return (
+		<ContextHeader title="Content overzicht">
+			<ContextHeaderTopSection>breadcrumbs</ContextHeaderTopSection>
+			<ContextHeaderActionsSection>
+				<Button iconLeft="plus">Nieuwe maken</Button>
+			</ContextHeaderActionsSection>
+		</ContextHeader>
+	);
 };
 
 export default ContentOverview;
