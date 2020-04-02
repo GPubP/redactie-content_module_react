1fae1d264031778ec3fa5ea0181908acef8e62c3
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-04-02 15:44:08 +0200

    chore: rename contentTypes.ts to contentTypes.service.ts

---
+++ public/lib/services/contentTypes/contentTypes.service.ts
@@ -0,0 +1,29 @@
+import apiService from '../api/api.service';
+
+import { ContentTypeSchema } from './contentTypes.service.types';
+
+// TODO: This data should come from the content type module API
+export const getContentType = async (uuid: string): Promise<ContentTypeSchema | null> => {
+	try {
+		const response: ContentTypeSchema = await apiService.get(`content-types/${uuid}`).json();
+
+		if (!response.fields) {
+			throw new Error('Failed to get content type');
+		}
+
+		const validateSchema = {
+			$schema: 'http://json-schema.org/draft-07/schema#',
+			type: 'object',
+			properties: response.validateSchema || {},
+		};
+		const errorMessages = response.errorMessages || {};
+		return {
+			...response,
+			validateSchema,
+			errorMessages,
+		};
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+};
