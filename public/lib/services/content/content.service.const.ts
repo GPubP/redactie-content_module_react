4fc9a12f44d006155274f69d2a68d72d88598df5
maximdegeyter <maximdegeyter@outlook.com>
2020-04-20 16:13:37 +0200

    feat: add search params to api call

---
+++ public/lib/services/content/content.service.const.ts
@@ -0,0 +1,6 @@
+import { SearchParams } from '../api/api.service.types';
+
+export const DEFAULT_VIEWS_SEARCH_PARAMS: SearchParams = {
+	page: 0,
+	pagesize: 10,
+};
--- public/lib/services/content/content.service.ts
+++ public/lib/services/content/content.service.ts
@@ -1,11 +1,23 @@
-import { api } from '../api';
+import { api, parseSearchParams, SearchParams } from '../api';
 
+import { DEFAULT_VIEWS_SEARCH_PARAMS } from './content.service.const';
 import { ContentCreateSchema, ContentSchema, ContentsSchema } from './content.service.types';
 
-export const getContent = async (): Promise<ContentSchema[] | null> => {
+export const getContent = async (
+	searchParams: SearchParams = DEFAULT_VIEWS_SEARCH_PARAMS
+): Promise<ContentsSchema | null> => {
 	try {
-		const response: ContentsSchema = await api.get('content').json();
-		return response.data;
+		const response: ContentsSchema = await api
+			.get(`content/content?${parseSearchParams(searchParams)}`)
+			.json();
+
+		if (!response.data) {
+			throw new Error('Failed to get content');
+		}
+
+		return {
+			data: response.data,
+		};
 	} catch (err) {
 		console.error(err);
 		return null;
