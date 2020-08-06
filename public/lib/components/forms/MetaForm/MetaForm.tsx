73ffb2cd6289cb69294da282a34b00e1a176dd13
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-08-06 19:49:25 +0200

    feat(metaForm): add validation schema

--- public/lib/components/forms/MetaForm/MetaForm.tsx
+++ public/lib/components/forms/MetaForm/MetaForm.tsx
@@ -4,7 +4,10 @@ import React, { FC, ReactElement } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 import { CompartmentProps } from '../../../api/api.types';
+import { ErrorMessage } from '../../../connectors/formRenderer';
 import FormikOnChangeHandler from '../FormikOnChangeHandler/FormikOnChangeHandler';
+
+import { MetaFormValidationSchema } from './MetaForm.const';
 
 const MetaForm: FC<CompartmentProps> = ({
 	contentValue,
@@ -22,77 +25,87 @@ const MetaForm: FC<CompartmentProps> = ({
 	return (
 		<Formik
 			innerRef={instance => formikRef && formikRef(instance)}
+			validationSchema={MetaFormValidationSchema}
 			onSubmit={onChange}
 			initialValues={value}
 		>
-			{({ submitForm }) => (
-				<>
-					<FormikOnChangeHandler onChange={submitForm} />
-					<h5 className="u-margin-bottom">Informatie</h5>
-					<p className="u-margin-bottom">Lorem Ipsum.</p>
-					<div className="row">
-						<div className="col-xs-12 col-md-6 u-margin-bottom">
-							<Field
-								type="text"
-								label="Werktitel"
-								name="label"
-								id="label"
-								placeholder="Typ een label"
-								required
-								as={TextField}
-							/>
-							<div className="u-text-light u-margin-top-xs">
-								Geef een werktitel op voor dit item.
-							</div>
-						</div>
-						{contentValue?.uuid && contentValue.uuid !== 'new' ? (
+			{({ submitForm, errors, touched }) => {
+				console.log(errors, 'errors');
+				return (
+					<>
+						<FormikOnChangeHandler onChange={submitForm} />
+						<h5 className="u-margin-bottom">Informatie</h5>
+						<p className="u-margin-bottom">Lorem Ipsum.</p>
+						<div className="row">
 							<div className="col-xs-12 col-md-6 u-margin-bottom">
-								<label className="a-input__label">UID</label>
-								<p className="u-text-light">
-									{contentValue?.uuid}
-									<CopyToClipboard text={contentValue?.uuid}>
-										<Link className="u-margin-left-xs" alt="copy to clipboard">
-											kopieer
-										</Link>
-									</CopyToClipboard>
-								</p>
+								<Field
+									type="text"
+									label="Werktitel"
+									name="label"
+									id="label"
+									placeholder="Typ een label"
+									required
+									as={TextField}
+								/>
+								<ErrorMessage name="label" />
+								<div className="u-text-light u-margin-top-xs">
+									Geef een werktitel op voor dit item.
+								</div>
 							</div>
-						) : null}
-					</div>
-					<div className="row">
-						<div className="col-xs-12 col-md-6 u-margin-bottom">
-							<Field
-								type="text"
-								label="Slug"
-								name="slug.nl"
-								id="slug"
-								placeholder="Typ een slug"
-								required
-								as={TextField}
-							/>
-							<div className="u-text-light u-margin-top-xs">
-								Bepaal de &apos;slug&apos; voor dit content item. Deze wordt
-								ondererandere gebruikt in de URL.
+							{contentValue?.uuid && contentValue.uuid !== 'new' ? (
+								<div className="col-xs-12 col-md-6 u-margin-bottom">
+									<label className="a-input__label">UID</label>
+									<p className="u-text-light">
+										{contentValue?.uuid}
+										<CopyToClipboard text={contentValue?.uuid}>
+											<Link
+												className="u-margin-left-xs"
+												alt="copy to clipboard"
+											>
+												kopieer
+											</Link>
+										</CopyToClipboard>
+									</p>
+								</div>
+							) : null}
+						</div>
+						<div className="row">
+							<div className="col-xs-12 col-md-6 u-margin-bottom">
+								<Field
+									type="text"
+									label="Slug"
+									name="slug.nl"
+									id="slug"
+									placeholder="Typ een slug"
+									required
+									as={TextField}
+								/>
+								<ErrorMessage name="slug.nl" />
+								<div className="u-text-light u-margin-top-xs">
+									Bepaal de &apos;slug&apos; voor dit content item. Deze wordt
+									ondererandere gebruikt in de URL.
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="row">
-						<div className="col-xs-12">
-							<Field
-								type="text"
-								label="Beschrijving"
-								name="description"
-								id="description"
-								placeholder="Typ een beschrijving."
-								as={Textarea}
-							/>
-							<div className="u-text-light u-margin-top-xs">
-								Geef dit item een korte beschrijving voor in het content overzicht.
+						<div className="row">
+							<div className="col-xs-12">
+								<Field
+									type="text"
+									label="Beschrijving"
+									name="description"
+									id="description"
+									placeholder="Typ een beschrijving."
+									as={Textarea}
+								/>
+								<div className="u-text-light u-margin-top-xs">
+									Geef dit item een korte beschrijving voor in het content
+									overzicht.
+								</div>
 							</div>
 						</div>
-					</div>
-				</>
-			)}
+					</>
+				);
+			}}
 		</Formik>
 	);
 };
