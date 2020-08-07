872b61657c99cfad27d3e55352ae9e1e2fce64b7
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-08-07 12:29:14 +0200

    feat: add error state for NavList item

--- public/lib/components/NavList/NavList.types.ts
+++ public/lib/components/NavList/NavList.types.ts
@@ -3,6 +3,7 @@ import { NavLinkProps } from 'react-router-dom';
 export interface NavListItem {
 	label: string;
 	to: NavLinkProps['to'];
+	hasError?: boolean;
 }
 
 export interface NavListProps {
