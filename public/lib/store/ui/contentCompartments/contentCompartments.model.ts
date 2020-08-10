5db70bd22552cf17f89a7c74ff0f33d0d1cdea6e
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-08-10 15:49:45 +0200

    feat(compartments): add isValid and validate properties to model

--- public/lib/store/ui/contentCompartments/contentCompartments.model.ts
+++ public/lib/store/ui/contentCompartments/contentCompartments.model.ts
@@ -1,6 +1,7 @@
 import { ActiveState, EntityState } from '@datorama/akita';
 import { FC } from 'react';
 
+import { ContentSchema } from '../../../api/api.types';
 import { CompartmentProps } from '../../../views/ContentForm/ContentForm.types';
 
 export type ModuleValue = any;
@@ -20,6 +21,8 @@ export interface ContentCompartmentModel {
 	slug?: string;
 	component: FC<CompartmentProps>;
 	type: CompartmentType;
+	isValid?: boolean;
+	validate?: (values: ContentSchema) => boolean;
 }
 
 export interface ContentCompartmentState
--- public/lib/views/ContentForm/ContentForm.const.ts
+++ public/lib/views/ContentForm/ContentForm.const.ts
@@ -1,7 +1,14 @@
-import { FieldsForm, MetaForm, StatusForm } from '../../components';
-import { CompartmentType } from '../../store/ui/contentCompartments';
+import { ContentSchema } from '../../api/api.types';
+import {
+	FieldsForm,
+	META_VALIDATION_SCHEMA,
+	MetaForm,
+	STATUS_VALIDATION_SCHEMA,
+	StatusForm,
+} from '../../components';
+import { CompartmentType, ContentCompartmentModel } from '../../store/ui/contentCompartments';
 
-export const INTERNAL_COMPARTMENTS = [
+export const INTERNAL_COMPARTMENTS: ContentCompartmentModel[] = [
 	{
 		label: 'Inhoud',
 		name: 'fields',
@@ -15,6 +22,8 @@ export const INTERNAL_COMPARTMENTS = [
 		slug: 'informatie',
 		component: MetaForm,
 		type: CompartmentType.INTERNAL,
+		isValid: false,
+		validate: (values: ContentSchema) => META_VALIDATION_SCHEMA.isValidSync(values.meta),
 	},
 	{
 		label: 'Status',
@@ -22,13 +31,17 @@ export const INTERNAL_COMPARTMENTS = [
 		slug: 'status',
 		component: StatusForm,
 		type: CompartmentType.INTERNAL,
+		isValid: false,
+		validate: (values: ContentSchema) => STATUS_VALIDATION_SCHEMA.isValidSync(values.meta),
 	},
 	// TODO: Implement planning when the workflow engine is implemented
 	// {
-	// 	label: 'planning',
+	// 	label: 'Planning',
 	// 	name: 'planning',
 	// 	slug: 'planning',
 	// 	component: PlanningForm,
 	// 	type: CompartmentType.INTERNAL,
+	// 	isValid: false,
+	// 	validate: (values: ContentSchema) => PLANNING_VALIDATION_SCHEMA.isValidSync(values),
 	// },
 ];
