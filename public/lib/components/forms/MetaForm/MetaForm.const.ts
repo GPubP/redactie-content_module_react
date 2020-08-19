a282a9d6e299c48089bc9e4a1bad7aa2c558f334
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-08-19 13:50:36 +0200

    feat(content-edit): remove label field from meta form

--- public/lib/components/forms/MetaForm/MetaForm.const.ts
+++ public/lib/components/forms/MetaForm/MetaForm.const.ts
@@ -1,7 +1,6 @@
 import { object, string } from 'yup';
 
 export const META_VALIDATION_SCHEMA = object().shape({
-	label: string().required('Gelieve een werktitel in te vullen'),
 	slug: object({
 		nl: string().required('Gelieve een slug in te vullen'),
 	}),
