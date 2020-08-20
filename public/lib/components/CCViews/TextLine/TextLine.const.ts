23e7e7bf6d169a749bbaa91c3b4cd29e662f27cb
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-08-20 11:20:08 +0200

    fix: pr remarks

--- public/lib/components/CCViews/TextLine/TextLine.const.ts
+++ public/lib/components/CCViews/TextLine/TextLine.const.ts
@@ -1,10 +1,1 @@
-export enum TextHTMLTypes {
-	H1 = 'h1',
-	H2 = 'h2',
-	H3 = 'h3',
-	H4 = 'h4',
-	H5 = 'h5',
-	H6 = 'h6',
-	DIV = 'div',
-	P = 'p',
-}
+export const TEXT_HTML_TYPES = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'p'];
--- public/lib/components/CCViews/TextLine/TextLine.tsx
+++ public/lib/components/CCViews/TextLine/TextLine.tsx
@@ -1,39 +1,20 @@
 import { ViewFieldProps } from '@redactie/form-renderer-module';
-import React, { FC, ReactElement } from 'react';
+import React, { FC } from 'react';
 
-import { TextHTMLTypes } from './TextLine.const';
+import { TEXT_HTML_TYPES } from './TextLine.const';
 
 const CCTextLineView: FC<ViewFieldProps> = ({ value }) => {
-	const { text, textHTMLType } = value;
+	const { text, textHTMLType: TextComponent } = value;
 
-	if (!text || !textHTMLType) {
+	if (!text || !TextComponent) {
 		return null;
 	}
 
-	const renderTextHTMLElement = (): ReactElement | null => {
-		switch (textHTMLType) {
-			case TextHTMLTypes.H1:
-				return <h1>{text}</h1>;
-			case TextHTMLTypes.H2:
-				return <h2>{text}</h2>;
-			case TextHTMLTypes.H3:
-				return <h3>{text}</h3>;
-			case TextHTMLTypes.H4:
-				return <h4>{text}</h4>;
-			case TextHTMLTypes.H5:
-				return <h5>{text}</h5>;
-			case TextHTMLTypes.H6:
-				return <h6>{text}</h6>;
-			case TextHTMLTypes.P:
-				return <p>{text}</p>;
-			case TextHTMLTypes.DIV:
-				return <div>{text}</div>;
-			default:
-				return null;
-		}
-	};
+	if (!TEXT_HTML_TYPES.includes(TextComponent)) {
+		return null;
+	}
 
-	return <>{renderTextHTMLElement()}</>;
+	return <TextComponent>{text}</TextComponent>;
 };
 
 export default CCTextLineView;
