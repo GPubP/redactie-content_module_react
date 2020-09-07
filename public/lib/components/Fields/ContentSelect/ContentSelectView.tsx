b83ccf9ee90dfcd7a1e120fd91dd04e594790769
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2020-09-07 18:01:58 +0200

    feat(contentSelect): add view mode

---
+++ public/lib/components/Fields/ContentSelect/ContentSelectView.tsx
@@ -0,0 +1,53 @@
+import { ViewFieldProps } from '@redactie/form-renderer-module';
+import React, { ReactElement, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import './ContentSelect.scss';
+
+import { MODULE_PATHS } from '../../../content.const';
+import TenantContext from '../../../context/TenantContext/TenantContext';
+import { useCcContentItem, useNavigate } from '../../../hooks';
+import { ccContentFacade } from '../../../store/ccContent';
+import { DataLoader } from '../../DataLoader';
+
+const ContentSelectView: React.FC<ViewFieldProps> = ({ value, fieldSchema }: ViewFieldProps) => {
+	const { siteId } = useContext(TenantContext);
+	const { generatePath } = useNavigate();
+	const [contentItemLoadingState, ccContentItem] = useCcContentItem();
+
+	useEffect(() => {
+		if (!value) {
+			return;
+		}
+		ccContentFacade.getContentItem(siteId, value);
+	}, [siteId, value]);
+
+	if (!value || typeof value !== 'string') {
+		return null;
+	}
+
+	const renderView = (): ReactElement | null => {
+		if (!ccContentItem) {
+			return null;
+		}
+
+		return (
+			<Link
+				id={`${fieldSchema?.name}-${value}`}
+				className="u-margin-bottom a-button a-button-primary has-icon-right"
+				title={ccContentItem?.meta.label}
+				to={generatePath(MODULE_PATHS.detailView, {
+					contentId: ccContentItem?.uuid,
+					siteId,
+				})}
+			>
+				<span className="fa fa-chevron-right"></span>
+				{ccContentItem?.meta.label}
+			</Link>
+		);
+	};
+
+	return <DataLoader loadingState={contentItemLoadingState} render={renderView}></DataLoader>;
+};
+
+export default ContentSelectView;
--- public/lib/components/Fields/ContentSelect/index.ts
+++ public/lib/components/Fields/ContentSelect/index.ts
@@ -1,1 +1,2 @@
 export { default as ContentSelect } from './ContentSelect';
+export { default as ContentSelectView } from './ContentSelectView';
