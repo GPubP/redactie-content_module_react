46825c3698c20903c7167983e243647316439ee9
Stefan Janssens <stefan.janssens@23seconds.be>
2020-10-23 11:05:01 +0200

    fix: add sort to default search params for content and content types

--- public/lib/services/content/content.service.const.ts
+++ public/lib/services/content/content.service.const.ts
@@ -5,6 +5,7 @@ import { ContentStatus } from './content.service.types';
 export const DEFAULT_CONTENT_SEARCH_PARAMS: SearchParams = {
 	skip: 0,
 	limit: 10,
+	sort: 'meta.label',
 };
 
 export const CONTENT_STATUS_TRANSLATION_MAP = {
