0e9e949e9a45bef5ae555cd3bd9899723eb41ab7
Stefan Janssens <stefan.janssens@23seconds.be>
2020-10-28 15:11:51 +0100

    fix: add missing value parameter for sorting table

--- public/lib/services/contentTypes/contentTypes.service.const.ts
+++ public/lib/services/contentTypes/contentTypes.service.const.ts
@@ -5,5 +5,4 @@ export const CONTENT_TYPES_PREFIX_URL = 'content/v1/proxy/content-types';
 export const DEFAULT_CONTENT_TYPES_SEARCH_PARAMS: SearchParams = {
 	skip: 0,
 	limit: 10,
-	sort: 'meta.label',
 };
