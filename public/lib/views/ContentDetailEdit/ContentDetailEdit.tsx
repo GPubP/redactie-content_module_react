aedaec75dc7e5717cc74d71a6f51ef039f7c41cd
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2021-05-10 10:03:46 +0200

    fix(content): omit meta

--- public/lib/views/ContentDetailEdit/ContentDetailEdit.tsx
+++ public/lib/views/ContentDetailEdit/ContentDetailEdit.tsx
@@ -7,7 +7,7 @@ import {
 	useNavigate,
 	useWorker,
 } from '@redactie/utils';
-import { equals } from 'ramda';
+import { equals, omit } from 'ramda';
 import React, { FC, ReactElement, useEffect, useMemo, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -44,7 +44,7 @@ const ContentDetailEdit: FC<ContentDetailChildRouteProps<ContentDetailEditMatchP
 	const [initialLoadingState, setInitialLoadingState] = useState(LoadingState.Loading);
 	const [hasChanges, resetDetectValueChanges] = useDetectValueChangesWorker(
 		!!contentItemDraft && initialLoadingState === LoadingState.Loaded,
-		contentItemDraft,
+		omit(['meta', 'versions'])(contentItemDraft),
 		BFF_MODULE_PUBLIC_PATH
 	);
 
