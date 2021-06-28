1b880ce75a253d469a095f5a16bac49bb7038c8d
smtsj <jo.smets@studiohyperdrive.be>
2021-06-28 13:06:29 +0200

    fix(planning): show archive message when publishing

--- public/lib/components/forms/PlanningForm/PlanningForm.const.ts
+++ public/lib/components/forms/PlanningForm/PlanningForm.const.ts
@@ -2,14 +2,15 @@ import { date, object, ObjectSchema } from 'yup';
 
 export const PLANNING_VALIDATION_SCHEMA = (publishTime: string): ObjectSchema<any> =>
 	object().shape({
-		publishTime: date().min(
-			new Date().toISOString(),
-			'Publicatiedatum moet in de toekomst liggen'
-		),
-		unpublishTime: date().min(
-			publishTime ? publishTime : new Date().toISOString(),
-			`Archiveringsdatum moet ${
-				publishTime ? 'na de publicatiedatum liggen' : 'in de toekomst liggen'
-			}`
-		),
+		publishTime: date()
+			.min(new Date().toISOString(), 'Publicatiedatum moet in de toekomst liggen')
+			.nullable(true),
+		unpublishTime: date()
+			.min(
+				publishTime ? publishTime : new Date().toISOString(),
+				`Archiveringsdatum moet ${
+					publishTime ? 'na de publicatiedatum liggen' : 'in de toekomst liggen'
+				}`
+			)
+			.nullable(true),
 	});
